fb7fe5ff238c646178ed00a2c3b9e3d2f87fcd87
Mike Salvatore <mike.s.salvatore@gmail.com>
2024-03-04 13:40:19 -0500

    Use an iterator for mock TCPPortSelector

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,11 @@ from unittest.mock import MagicMock
 
 import pytest
 from agentpluginapi import ITCPPortSelector
+from monkeytypes import NetworkPort
 
 
 @pytest.fixture(scope="session")
 def tcp_port_selector():
-    return MagicMock(spec=ITCPPortSelector)
+    tcp_port_selector = MagicMock(spec=ITCPPortSelector)
+    tcp_port_selector.get_free_tcp_port.side_effect = (NetworkPort(p) for p in range(54321, 65536))
+    return tcp_port_selector
